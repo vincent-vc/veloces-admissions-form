56059e5c6267c2dad6ff52384ef3489863df87a3
vincent vc <49089186+vincent-vc@users.noreply.github.com>
2025-12-24 08:25:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("sendOtp").addEventListener("click", function () {
-  document.querySelector(".otp").style.display = "block";
+  document.getElementById("otpBox").style.display = "block";
+  alert("OTP sent (demo)");
+});
+
+document.getElementById("admissionForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Form submitted successfully (demo)");
 });
